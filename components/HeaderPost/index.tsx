87127cf316b300f481c319422b30fd2917a1fa76
64J0 <vinigaio97@gmail.com>
2021-05-08 16:49:10 -0300

    feat: enhance HeaderPost component, enhance the HeaderPost component by adjusting the classname used in the CSS, see also: #12

--- components/HeaderPost/index.tsx
+++ components/HeaderPost/index.tsx
@@ -2,27 +2,26 @@ import React from "react";
 import Link from "next/link";
 import { FiHome, FiChevronLeft } from "react-icons/fi";
 
-import styles from "../Layout/styles.module.scss";
-import utilStyles from "../../styles/utils.module.css";
-
 import headerStyles from "./styles.module.scss";
 
 const HeaderPost: React.FC = () => {
   return (
-    <div className={headerStyles.container}>
+    <div className={headerStyles['header-post']}>
       <Link href="/contato">
         <a>
           <img
             src="/images/profile.jpg"
-            className={`${styles.headerImage} ${utilStyles.borderCircle} ${headerStyles.contactLink}`}
+            className={headerStyles['header-post__image']}
             alt={"Uma foto minha"}
           />
         </a>
       </Link>
-      <h2 className={headerStyles.backToHome}>
+      <h2 className={headerStyles['header-post__back-home']}>
         <Link href="/">
           <a>
-            <FiChevronLeft size="4rem" className={headerStyles.moveToLeft} />
+            <FiChevronLeft size="4rem"
+              className={headerStyles['header-post__move-left']}
+            />
             <FiHome size="4rem" />
           </a>
         </Link>
