232a896c5065d6d6c9a592dca1cee71594f78f3c
64J0 <vinigaio97@gmail.com>
2021-05-08 16:58:33 -0300

    feat: update Layout component, enhance the Layout component to use some more semantic CSS styles, see also: #12

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React from "react";
 import { FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
 
 import HeaderHome from "../HeaderHome";
@@ -14,34 +14,32 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children, home }) => {
   return (
-    <>
-      <div className={styles['layout-container']}>
-        <header className={styles.header}>
-          {home ? (<HeaderHome />) : (<HeaderPost />)}
-        </header>
+    <div className={styles.layout}>
+      <header className={styles['layout__head']}>
+        {home ? <HeaderHome /> : <HeaderPost />}
+      </header>
 
-        <main className={styles.main}>
-          {children}
-        </main>
+      <main className={styles['layout__body']}>
+        {children}
+      </main>
 
-        <footer className={styles.footer}>
-          <span>
-            &copy; Vinícius Gajo Marques Oliveira. All rights reserved.
+      <footer className={styles['layout__footer']}>
+        <span>
+          &copy; Vinícius Gajo Marques Oliveira. All rights reserved.
           </span>
-          <span className={styles['icon-container']}>
-            <a href="https://twitter.com/viniciusgajo">
-              <FaTwitter size="1.8rem" className={styles.item} />
-            </a>
-            <a href="https://github.com/64J0">
-              <FaGithub size="1.8rem" className={styles.item} />
-            </a>
-            <a href="https://www.linkedin.com/in/vinicius-gajo/">
-              <FaLinkedin size="1.8rem" className={styles.item} />
-            </a>
-          </span>
-        </footer>
-      </div>
-    </>
+        <span className={styles['social__container']}>
+          <a href="https://twitter.com/viniciusgajo">
+            <FaTwitter size="1.8rem" className={styles['social__item']} />
+          </a>
+          <a href="https://github.com/64J0">
+            <FaGithub size="1.8rem" className={styles['social__item']} />
+          </a>
+          <a href="https://www.linkedin.com/in/vinicius-gajo/">
+            <FaLinkedin size="1.8rem" className={styles['social__item']} />
+          </a>
+        </span>
+      </footer>
+    </div>
   );
 }
 
