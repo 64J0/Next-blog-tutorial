755cbe89ac0ec6086f485307dcd20e510221764d
64J0 <vinigaio97@gmail.com>
2021-05-22 22:24:42 -0300

    feat: enhance the 404 page, see also: #15

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,27 @@
 import React from "react";
+import Head from "next/head";
+import Image from "next/image";
+
+import Layout from "../components/Layout";
 
 // pages/404.js
 const Custom404: React.FC = () => {
-  return <h1>404 - Página não encontrada</h1>;
+  return (
+    <Layout home="">
+      <Head>
+        <title>404</title>
+      </Head>
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "4rem" }}>
+        <h1>404 - Página não encontrada</h1>
+        <Image
+          src="/images/404-image.png"
+          alt="Image of a broken robot"
+          width={400}
+          height={300}
+        />
+      </div>
+    </Layout>
+  );
 }
 
 export default Custom404;
