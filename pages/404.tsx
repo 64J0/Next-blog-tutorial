e69a7a7a6b02f17a581536134b50cca55208fa18
64J0 <vinigaio97@gmail.com>
2021-05-22 22:23:45 -0300

    Revert "feat: enhance the 404 page"
    This reverts commit 228ad1b4794e00eebcfbe13295bee46d0840d2e3.

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,27 +1,8 @@
 import React from "react";
-import Head from "next/head";
-import Image from "next/image";
-
-import Layout from "../components/Layout";
 
 // pages/404.js
 const Custom404: React.FC = () => {
-  return (
-    <Layout home="">
-      <Head>
-        <title>404</title>
-      </Head>
-      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "4rem" }}>
-        <h1>404 - Página não encontrada</h1>
-        <Image
-          src="/images/404-image.png"
-          alt="Image of a broken robot"
-          width={400}
-          height={300}
-        />
-      </div>
-    </Layout>
-  );
+  return <h1>404 - Página não encontrada</h1>;
 }
 
 export default Custom404;
